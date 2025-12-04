a742601b6b129f655798955c2b0e47cfa247d67b
YZZlD <max.verhulst@hotmail.com>
2025-12-04 14:09:34 -0700

    feat: date validation, form handling, email notifications

--- emailEmitter.js
+++ emailEmitter.js
@@ -1,10 +1,33 @@
 const EventEmitter = require('events');
+const transporter = require('./transporter.js');
 
 const emailEmitter = new EventEmitter();
 
 emailEmitter.on('email', (data) => {
     //THIS NEEDS TO CHANGE TO A PROPER IMPLEMENTATION FOR EMAILING ORDER CONFIRMATION
-    console.log(data);
+    const customFormatter = new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true, // Use 12-hour format with AM/PM
+    });
+
+    const text = data.returnDatetime ? `This is confirmation of your booking for a round trip flight. Departure at: ${customFormatter.format(new Date(data.departureDatetime))}. Return at: ${customFormatter.format(new Date(data.returnDatetime))}`
+    : `This is a confirmation of your booking for a single flight. Departure at: ${customFormatter.format(new Date(data.departureDatetime))}.`;
+
+    const emailOptions = {
+        from: 'Angelina Botsford <angelina.botsford@ethereal.email>',
+        to: `${data.email}`,
+        subject: 'Airline booking confirmation',
+        text: text
+    };
+
+    transporter.sendMail(emailOptions, (err, info) => {
+        if(err) console.log('Error occured. ' + err.message);
+    });
 });
 
 module.exports = emailEmitter;
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const transport = require('./transporter.js');
 const emailEmitter = require('./emailEmitter.js');
 require('dotenv').config();
 
@@ -13,6 +12,20 @@ app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'html/bookings.html'));
+});
+
+app.post('/reservation', (req, res) => {
+    const {email, departureDatetime, returnDatetime} = req.body;
+
+    const now = new Date(Date.now());
+
+    if(!(new Date(departureDatetime) > now)) return res.sendFile(path.join(__dirname, 'public', 'html/invalidBooking.html'));
+
+    if(returnDatetime && !(new Date(returnDatetime) > now)) return res.sendFile(path.join(__dirname, 'public', 'html/invalidBooking.html'));
+
+    emailEmitter.emit("email", {email: email, departureDatetime: departureDatetime, returnDatetime: returnDatetime});
+
+    res.sendFile(path.join(__dirname, 'public', 'html/validBooking.html'));
 });
 
 app.listen(process.env.PORT, () => {
